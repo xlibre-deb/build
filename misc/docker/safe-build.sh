ed8bc5a0dcc6d911f6e4c41074ebf3273c249022
apebl <a@pebl.cc>
2025-09-28 12:33:03 +0000

    build: Capture stderr in build log

--- misc/docker/safe-build.sh
+++ misc/docker/safe-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o pipefail
 
-if SYSTEMD=${SYSTEMD} bash /build/build.sh /build | tee /tmp/build.log; then
+if SYSTEMD=${SYSTEMD} bash /build/build.sh /build 2>&1 | tee /tmp/build.log; then
   rm -rf /build/build.sh /build/safe-build.sh /build/*/
   echo success > /build/build-status
 else
